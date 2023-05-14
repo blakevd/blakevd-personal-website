f930f2c1b683a6ad38f90f98984d4729cecb9e16
iPupkin <blakeyvd@gmail.com>
2023-05-13 22:02:32 -0600

    fixed home button overlay covering it

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './images/home-icon.svg';
 import './App.css';
 // web routing
-import { BrowserRouter as Router, Route, Link, Routes, NavLink } from'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink } from'react-router-dom';
 import NotFound from './pages/NotFound'
 import Contact from './pages/Contact'
 import Projects from './pages/Projects'
@@ -12,27 +12,29 @@ import About from './pages/About'
 import { useState } from 'react';
 
 function App() {
-  const [currPage, setCurrPage] = useState<string>("About");
+
   return (
     <div>
       <div className="overlay"></div>
       <div className="wrapper">
       <Router>
           <header>
-              <a href="#"><img src={logo} width="40" height="auto" alt="Blake Van Dyken" /></a>
-              <nav>
-                  <ul>
-                      <li>
-                        <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>About</NavLink>
-                      </li>
-                      <li>
-                        <NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>Projects</NavLink>
-                      </li>
-                      <li>
-                        <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Contact</NavLink>
-                      </li>
-                  </ul>
-              </nav>
+            <NavLink to="/">
+              <img src={logo} width="40" height="auto" alt="Blake" />
+            </NavLink>
+            <nav>
+                <ul>
+                    <li>
+                      <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>About</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>Projects</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Contact</NavLink>
+                    </li>
+                </ul>
+            </nav>
           </header>
 
           <main>
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -5,9 +5,9 @@ const NotFound = () => {
         <div>
             <h1>Oops! You seem to be lost.</h1>
             <p>Here are some helpful links:</p>
-            <Link to='/'>Home</Link>
-            <Link to='/blog'>Blog</Link>
-            <Link to='/contact'>Contact</Link>
+            <Link to='/'>Home</Link><br/>
+            <Link to='/projects'>Projects</Link><br/>
+            <Link to='/contact'>Contact</Link><br/>
         </div>
     )
 }
